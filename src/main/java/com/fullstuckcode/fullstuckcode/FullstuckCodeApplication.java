e1ae838d4afe9837d8943d39e35fa71a15d75af1
Rifqi Ramadhan <rifqyomp@gmail.com>
2022-04-06 16:42:15 +0700

    initial commit

---
+++ src/main/java/com/fullstuckcode/fullstuckcode/FullstuckCodeApplication.java
@@ -0,0 +1,13 @@
+package com.fullstuckcode.fullstuckcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstuckCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstuckCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstuckcode/fullstuckcode/FullstuckCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstuckcode.fullstuckcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstuckCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
